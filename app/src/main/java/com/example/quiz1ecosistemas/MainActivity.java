ac7d3630e56ed74e96c577b0bf9991260a90d3fb
Brand28 <alejandrobrand28@gmail.com>
2021-09-14 21:56:22 -0500

    pagina principal creada

--- app/src/main/java/com/example/quiz1ecosistemas/MainActivity.java
+++ app/src/main/java/com/example/quiz1ecosistemas/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.quiz1ecosistemas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtHead;
+    private TextView txtTitle;
+
+    private ConstraintLayout container_big;
+    private ConstraintLayout container_text;
+
+    private Button bnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtHead = findViewById(R.id.txtHead);
+        txtTitle = findViewById(R.id.txtTitle);
+
+        container_big = findViewById(R.id.container_big);
+        container_text = findViewById(R.id.container_text);
+
+        bnRegister = findViewById(R.id.bnRegister);
+
+        bnRegister.setOnClickListener(
+                (v) -> {
+                    //Intent i = new Intent();
+                }
+        );
+
     }
 }
